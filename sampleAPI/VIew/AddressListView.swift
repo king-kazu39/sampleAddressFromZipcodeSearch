3024f48bba7cea7aae8645e100f3bc0022c9689b
kazuya <kazuya@kazuyanoMacBook-Pro.local>
2021-01-29 01:12:22 +0900

    fix #11 登録画面のレイアウト作成

---
+++ sampleAPI/VIew/AddressListView.swift
@@ -0,0 +1,43 @@
+//
+//  AddressListView.swift
+//  sampleAPI
+//
+//  Created by kazuya on 2021/01/29.
+//
+
+import SwiftUI
+
+struct AddressListView: View {
+    @Binding var addressStrings: [String]
+    @ObservedObject var createVM: CreateViewModel
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button(action: {
+                    self.presentationMode.wrappedValue.dismiss()
+                }) {
+                    Text("Close")
+                        .foregroundColor(.red)
+                        .padding()
+                }
+            }
+            List(addressStrings, id: \.self) { address in
+                Button(action: {
+                    self.presentationMode.wrappedValue.dismiss()
+                    self.createVM.addressString = address
+                }) {
+                    Text("\(address)")
+                }
+            }
+        }
+    }
+}
+
+struct AddressListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressListView(addressStrings: .constant(["南風原町","西原町"]), createVM: CreateViewModel())
+    }
+}
