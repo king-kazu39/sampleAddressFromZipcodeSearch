bfe9328942d9372d645ef8c4e892b4fbc497c455
kazuya <kazuya@kazuyanoMacBook-Pro.local>
2021-01-29 00:39:55 +0900

    fix #10 プロフィール一覧作成

--- sampleAPI/VIew/ProfileListView.swift
+++ sampleAPI/VIew/ProfileListView.swift
@@ -10,8 +10,17 @@ import SwiftUI
 struct ProfileListView: View {
     var body: some View {
         NavigationView {
-            Text("プロフィール一覧")
-                .navigationBarTitle("プロフィール一覧", displayMode: .inline)
+            ScrollView(.vertical, showsIndicators: false) {
+                ForEach(0..<10) {_ in
+                    LazyVStack {
+                        ProfileCell(username: "愛知太郎")
+                    }
+                }
+                .padding(.top, 20)
+                .padding(.bottom, 30)
+            }.frame(width: UIScreen.main.bounds.width - 30,
+                    height: .infinity)
+            .navigationBarTitle("プロフィール一覧", displayMode: .inline)
         }
     }
 }
